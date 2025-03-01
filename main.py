60bb59992a00f10eb7f82cae24b7c5c2f3104325
cmatias <cmatias@cofares.es>
2025-03-01 13:08:43 +0100

    Creation of model trainer stage

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from src.datascience import logger
 from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.datascience.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 from src.datascience.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
+from src.datascience.pipeline.model_trainer_pipeline import ModelTrainerTrainingPipeline
+
+
+########## Data Ingestion Stage ############
 
 STAGE_NAME="Data Ingestion Stage"
 try:
@@ -13,6 +17,8 @@ except Exception as e:
     logger.exception(e)
     raise e
 
+########## Data Validation Stage ############
+
 STAGE_NAME = "Data Validation Stage"
 try:
     logger.info(f">>>>>> Stage {STAGE_NAME} started <<<<<<")
@@ -23,11 +29,25 @@ except Exception as e:
     logger.exception(e)
     raise e
 
+########## Data Transformation Stage ############
+
 STAGE_NAME = "Data Transformation Stage"
 try:
     logger.info(f">>>>>> Stage {STAGE_NAME} started <<<<<<")
     data_transformation = DataTransformationTrainingPipeline()
     data_transformation.initiate_data_transformation()
+    logger.info(f">>>>>> Stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+########## Model Training Stage ############
+
+STAGE_NAME = "Model Training Stage"
+try:
+    logger.info(f">>>>>> Stage {STAGE_NAME} started <<<<<<")
+    model_trainer = ModelTrainerTrainingPipeline()
+    model_trainer.initiate_model_training()
     logger.info(f">>>>>> Stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
---
+++ src/datascience/components/model_trainer.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import os
+from src.datascience import logger
+from sklearn.linear_model import ElasticNet
+import joblib
+from src.datascience.entity.config_entity import ModelTrainerConfig
+
+class ModelTrainer:
+    def __init__(self, config: ModelTrainerConfig):
+        self.config = config
+    
+    def train(self):
+        train_data = pd.read_csv(self.config.train_data_path)
+        test_data = pd.read_csv(self.config.test_data_path)
+
+        X_train = train_data.drop(columns=[self.config.target_columnn], axis=1)
+        y_train = train_data[self.config.target_columnn]
+
+        X_test = test_data.drop(columns=[self.config.target_columnn], axis=1)
+        y_test = test_data[self.config.target_columnn]
+
+        lr = ElasticNet(alpha=self.config.alpha, l1_ratio=self.config.l1_ratio, random_state=42)
+        lr.fit(X_train, y_train)
+
+        joblib.dump(lr, os.path.join(self.config.root_dir, self.config.model_name))
